bf0e1c48470e10c46ae1e95d3d804daa0615edd2
hansen1416 <badapplesweetie@gmail.com>
2024-10-29 23:40:17 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
+
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -19,6 +24,8 @@ function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   renderer.render(scene, camera);
+
+  controls.update();
 }
 
 animate();
