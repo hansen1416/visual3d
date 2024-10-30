d1ffcec7478fececcfdb92e02a43ef80cc225aaa
hansen1416 <badapplesweetie@gmail.com>
2024-10-30 23:20:36 +0800

    1

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,9 +1,23 @@
+use std::path::Path;
+
 #[tauri::command]
 pub fn start_command() {
     println!("Command starts!!!, command");
 }
 
 #[tauri::command(rename_all = "snake_case")]
-pub fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name).into()
+pub fn greet(filename: &str) {
+    // format!("Hello, {}! You've been greeted from Rust!", name).into()
+
+    let path: &str = "path/to/your/file.txt";
+
+    // Create a Path object
+    let file_path: &Path = Path::new(filename);
+
+    // Check if the file exists
+    if file_path.exists() {
+        println!("The file exists at the specified path: {}", path);
+    } else {
+        println!("The file does not exist at the specified path: {}", path);
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,38 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // When using the Tauri API npm package:
 import { invoke } from '@tauri-apps/api/core';
 
-import { BaseDirectory, exists, create } from '@tauri-apps/plugin-fs';
+import { BaseDirectory, exists, readFile } from '@tauri-apps/plugin-fs';
 
 
 const is_file_exists = await exists('wham_output.pkl', {
   baseDir: BaseDirectory.Document,
 });
 
-console.log(is_file_exists);
+if (is_file_exists) {
+  const data = await readFile('wham_output.pkl', {
+    baseDir: BaseDirectory.Document,
+  });
+
+  console.log(data);
+}
+
 
 // Invoke the command
 invoke('start_command');
 
-invoke('greet', { name: 'Hansen' }).then((response) => {
-  console.log(response);
-});
+// invoke('greet', { name: 'Hansen' }).then((response) => {
+//   console.log(response);
+// });
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
 
+const canvas: HTMLCanvasElement = document.getElementById('three_scene') as HTMLCanvasElement;
 
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
