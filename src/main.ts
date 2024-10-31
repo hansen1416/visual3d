d8ab43176c66f0c6a765ce0723a4ac4e16921112
hansen1416 <badapplesweetie@gmail.com>
2024-10-31 18:05:16 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,8 @@ invoke('start_command');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+camera.position.z = 6;
+
 const canvas: HTMLCanvasElement = document.getElementById('three_scene') as HTMLCanvasElement;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
@@ -25,23 +27,30 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
 const vertices = await vertices_loader('all_vertices.bin');
 const faces = await faces_loader('faces.bin');
 
-console.log(vertices);
-console.log(faces);
+
+const geometry = new THREE.BufferGeometry();
+
+const vertices0 = new Float32Array(vertices[0].flat());
+const indices = faces.flat();
+
+console.log(vertices0);
+console.log(indices);
+
+geometry.setIndex(indices);
+geometry.setAttribute('position', new THREE.BufferAttribute(vertices0, 3));
+
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const smpl_mesh = new THREE.Mesh(geometry, material);
+
+scene.add(smpl_mesh);
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 
   controls.update();
